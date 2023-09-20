d28bc7a589320548a7e6b7339538945c3e99bd54
Fic-1 <filip.brkovic1@gmail.com>
2023-09-20 17:35:57 +0200

    designed a card

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,3 +2,13 @@ import { hello as hello } from "./model";
 hello();
 const newVar = "string";
 console.log(newVar);
+
+const fetchWord = async function () {
+  const res = await fetch(
+    "https://api.dictionaryapi.dev/api/v2/entries/en/food"
+  );
+  const [data] = await res.json();
+  console.log(data);
+};
+
+fetchWord();
