2fc444fd334f258ea85c1062b745fe6568d4a301
Fic-1 <filip.brkovic1@gmail.com>
2023-09-23 11:40:13 +0200

    active word design

--- src/js/model.js
+++ src/js/model.js
@@ -35,7 +35,7 @@ export const getWord = async function (word) {
       `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
     );
     const [data] = await res.json();
-    // console.log(data);
+    console.log(data);
     if (!data.word) throw new Error('Unknown word');
     state.words.push(await createWordObject(data));
   } catch (error) {
