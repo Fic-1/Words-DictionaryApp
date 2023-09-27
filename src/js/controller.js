86e15f6552d07c34f93c955e84b5d1acbcfae3fd
Fic-1 <filip.brkovic1@gmail.com>
2023-09-27 15:35:27 +0200

    cleaned up

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,42 +1,8 @@
 import * as model from './model';
 import wordView from './views/wordView';
 import searchView from './views/searchView';
-import cardView from './views/activeWordView';
 import activeWordView from './views/activeWordView';
 import savedWordsView from './views/savedWordsView';
-
-const btnSearch = document.querySelector('.search--button');
-const searchBar = document.querySelector('.search--bar');
-const wordDiv = document.querySelector('.word-div');
-
-// const getWord = async function () {
-//   model.createWordObject(searchBar.value)
-// };
-
-// VIEW
-btnSearch.addEventListener('click', function (e) {
-  e.preventDefault();
-});
-const controlRecipes = async function () {
-  try {
-    const id = window.location.hash.slice(1);
-    if (!id) return;
-    recipeView.renderSpinner();
-    // 0) Update results view to mark selected search result
-    resultsView.update(model.getSearchResultsPage());
-
-    //3.) Updating bookmarks view
-    bookmarksView.update(model.state.bookmarks);
-
-    // 1.) Loading recipe
-    await model.loadRecipe(id);
-    // 2.) Rendereing the recipe
-    recipeView.render(model.state.recipe);
-  } catch (error) {
-    recipeView.renderError();
-    console.error(error);
-  }
-};
 
 const renderWords = async function (query) {
   try {
@@ -76,6 +42,7 @@ const controlActiveSave = function (id) {
     word => word.ID === +id
   );
   activeWordView.render(savedWordLink);
+  model.activeWord = savedWordLink;
 
   // activeWordView.render(model.state.savedWords.filter(word => word.ID === id));
 };
